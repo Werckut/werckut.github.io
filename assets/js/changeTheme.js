6908b395bd5997b73c35125adbea1ae8c7dd2b88
Werckut <Sergei_sa@list.ru>
2021-04-20 16:16:24 +0300

    Срочное включение

--- assets/js/changeTheme.js
+++ assets/js/changeTheme.js
@@ -2,12 +2,12 @@ let themeSwitchButton = document.getElementById("switchThemeButton");
 
 themeSwitchButton.onclick = () => {
     let theme = document.getElementById("themeType");
-    if (theme.getAttribute("href") == "../assets/style/css/themes/light-theme.css") {
-        theme.href = "../assets/style/css/themes/dark-theme.css";
+    if (theme.getAttribute("href") == "../assets/styles/css/themes/light-theme.css") {
+        theme.href = "../assets/styles/css/themes/dark-theme.css";
         themeSwitchButton.innerHTML = "Светлая тема";
     }
     else {
-        theme.href = "../assets/style/css/themes/light-theme.css"
+        theme.href = "../assets/styles/css/themes/light-theme.css"
         themeSwitchButton.innerHTML = "Тёмная тема";
     }
 }
